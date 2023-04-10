4cc77d518452cda5fe4a4b30891d3a88d5e0c72f
Andy Terhune <aterhune1984@gmail.com>
2023-04-10 11:36:12 -0400

    modifying yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = "cache.t4g.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis7.cluster.on"
+  parameter_group_name = "default.redis7.x"
   engine_version       = "7.0.7"
   port                 = 6379
 }
