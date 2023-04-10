5c7ca954ec1158ff50d802943a4141e2cb67d9e7
Andy Terhune <aterhune1984@gmail.com>
2023-04-10 10:18:56 -0400

    modifying yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "test_instance" {
- ami           = "ami-830c94e3"
+ ami           = "ami-06e46074ae430fba6"
  instance_type = "t2.nano"
  tags = {
    Name = "test_instance"
