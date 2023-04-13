8f2df6456b3821e30f89b44acccb4c1ea0480b2b
Andy Terhune <aterhune1984@gmail.com>
2023-04-13 15:36:09 -0400

    updating eks versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,7 +83,7 @@ module "eks" {
 }
     
 
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+#  https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
