e8bd97d3ac43452765d0f3fbc155b092b0c34fbc
Andy Terhune <aterhune1984@gmail.com>
2023-04-13 10:33:17 -0400

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    region = "us-east-2"
+    key    = "terraform.tfstate"
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
