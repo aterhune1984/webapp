fa7596c58dd214c3ea3b5d3f01dabf0adca4aa56
Andy Terhune <aterhune1984@gmail.com>
2023-04-13 15:57:12 -0400

    updating eks versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,17 +63,17 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.nano"]
+      instance_types = ["t2.small"]
 
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      max_size     = 3
+      desired_size = 2
     }
 
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.nano"]
+      instance_types = ["t2.small"]
 
       min_size     = 1
       max_size     = 2
