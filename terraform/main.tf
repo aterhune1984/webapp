9e17db0891e7a436f77083a4a0bb0416e54b1d94
Andy Terhune <aterhune1984@gmail.com>
2023-04-13 08:53:27 -0400

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,32 +15,32 @@ provider "aws" {
  region = "us-east-2"
 }
 
-# Define the VPC for the EKS cluster
-resource "aws_vpc" "example_vpc" {
+resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
-
   tags = {
     Name = "example-vpc"
   }
 }
 
-# Define the private subnets for the EKS cluster
-
-
-resource "aws_subnet" "private" {
-  count = 2
-  vpc_id = aws_vpc.example_vpc.id
-  cidr_block = cidrsubnet(aws_vpc.example_vpc.cidr_block, 4, count.index + 1)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+resource "aws_subnet" "example" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
   tags = {
-    Name = "${var.cluster_name}-private-${count.index + 1}"
+    Name = "example-subnet"
   }
 }
 
-# Define the IAM role for the EKS cluster
-resource "aws_iam_role" "eks_cluster" {
-  name = "example-eks-cluster"
+resource "aws_eks_cluster" "example" {
+  name = "example-cluster"
+  role_arn = aws_iam_role.example.arn
+  vpc_config {
+    subnet_ids = [aws_subnet.example.id]
+  }
+}
 
+resource "aws_iam_role" "example" {
+  name = "example-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -55,45 +55,7 @@ resource "aws_iam_role" "eks_cluster" {
   })
 }
 
-# Define the IAM policy for the EKS cluster
-resource "aws_iam_policy" "eks_cluster" {
-  name        = "example-eks-cluster"
-  policy      = data.aws_iam_policy_document.eks_cluster.json
-}
-
-# Define the IAM policy attachment for the EKS cluster
-resource "aws_iam_role_policy_attachment" "eks_cluster" {
-  policy_arn = aws_iam_policy.eks_cluster.arn
-  role       = aws_iam_role.eks_cluster.name
-}
-
-# Define the data source for the EKS cluster IAM policy document
-data "aws_iam_policy_document" "eks_cluster" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "eks:DescribeCluster",
-      "eks:ListClusters",
-    ]
-
-    resources = [
-      aws_eks_cluster.example_cluster.arn,
-    ]
-  }
-}
-
-# Define the EKS cluster
-resource "aws_eks_cluster" "example_cluster" {
-  name     = "example-cluster"
-  role_arn = aws_iam_role.eks_cluster.arn
-
-  vpc_config {
-    subnet_ids = aws_subnet.private.*.id
-    vpc_id     = aws_vpc.example_vpc.id
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster,
-  ]
+resource "aws_iam_role_policy_attachment" "example" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.example.name
 }
