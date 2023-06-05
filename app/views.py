53a73f76b0e85bd88066133e460b5d4fcc838999
Andy Terhune <aterhune1984@gmail.com>
2023-06-05 16:09:52 -0400

    testing /metrics endpoing

--- app/views.py
+++ app/views.py
@@ -4,10 +4,7 @@ from flask import render_template
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
+@metrics.counter('hello_world')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
-
-@app.route('/metrics')
-def myMetrics():
-    return metrics.registry
